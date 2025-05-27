9a5400f927a862295abcd5ccd8486040a8fcb660
ana laura <ana.lrsacramento@senacsp.edu.br>
2025-05-27 14:33:54 -0300

    commit1

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -1,16 +1,43 @@
 package util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class Conexao {
 
-    private String driver = "com.mysql.cj.jdbc.Driver";
-    private String url = "jdbc:mysql://127.0.0.1:3306/hotel";
-    private String usuario = "dbaHotel";
-    private String senha = "dbaHotel123";
+    private String driver;
+                                //jdbc:mysql://localhost:3306/dbHotel
+    private String url;
+    private String usuario;
+    private String senha;
+
+    /*inicializar um construtor, de modo que quando essa classe Conexão.java instanciar um
+    objeto, o construtor será executado e a classe Conexao.java inicializada */
+
+    public Conexao() {
+        carregarConfiguracoes();
+    }
+
+    //metodo com parametros de configuração das variaveis de ambiente
+    private void carregarConfiguracoes() {
+        Properties props = new Properties();
+        try (InputStream inputPropsConfig = getClass().getClassLoader().getResourceAsStream
+                ("config.properties")) {
+            props.load(inputPropsConfig);
+            driver = props.getProperty("driver");
+            url = props.getProperty("url");
+            usuario = props.getProperty("usuario");
+            senha = props.getProperty("senha");
+        }
+        catch (IOException erro) {
+            System.out.println("Erro ao carregar configurações: " + erro.getMessage());
+        }
+    }
 
     public Connection conectar() {
         Connection condb = null;
